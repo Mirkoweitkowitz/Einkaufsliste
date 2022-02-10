e2ce59eafe2fb24ccaa8f7397c6c0fc02024ac66
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-02-10 14:47:37 +0100

    alles geändert

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ helloWorld();
 // Return value
     return 0;
 }
+function helloWorld() {
+    // This line prints a simple message
+    console.log("Hello World");
+    // Return value
+    return 0;
+}
+function main() {
+    console.log("Calling the function: ");
+
+    // Function call
+    helloWorld();
+
+    // Return value
+    return 0;
+}
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+The front page of my web panel
+<div>
+    <p> This is a paragraph in a div with a <br> linebreak </p>
+</div>
+</body>
+</html>
